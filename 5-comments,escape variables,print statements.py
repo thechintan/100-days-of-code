0dd96e0ad50328fe0cc023c4ffa735498785a6ea
Chintan Boghani <chintanboghani99@gmail.com>
2025-06-16 20:12:52 +0530

    Add files via upload

---
+++ 5-comments,escape variables,print statements.py
@@ -0,0 +1,15 @@
+'''
+ learning 
+ comments
+ shortcut in replit,it's
+ ctrl + /
+'''
+
+
+print('hello world\n7')
+print('hey i am a \'good boy\' and he is also good boy')
+print('hey',6,7,sep='**',end='009')
+print('Chintan')
+a='Chintan'
+b='Boghani'
+print(a,""+b)
